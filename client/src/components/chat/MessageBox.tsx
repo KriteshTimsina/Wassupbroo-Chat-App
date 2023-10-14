f3224be0e46acf31f641f06859ebff8488b58354
Namatuzio <lemonadewalkerz@gmail.com>
2023-10-13 22:50:53 -0400

    Emoji Keyboard Implemented
    Utilized Emoji-Mart (https://github.com/missive/emoji-mart) to implement an emoji picker into the message box area.

--- client/src/components/chat/MessageBox.tsx
+++ client/src/components/chat/MessageBox.tsx
@@ -5,9 +5,23 @@ import { MdOutlineAttachment, MdOutlineEmojiEmotions } from "react-icons/md";
 import { TbSend } from "react-icons/tb";
 import { AiFillLike } from "react-icons/ai";
 
+import data from '@emoji-mart/data'
+import Picker from '@emoji-mart/react'
+import { useState } from "react";
+
 const MessageBox = () => {
   const { message, handleSetMessage, handleSendMessage, sendThumpsUp } =
     useSocket();
+  const [emojiPickerVisible, setEmojiPickerVisible] = useState(false);
+
+  const handleEmojiSelect = (emoji: any) => {
+    message.text += emoji.native;
+    handleSetMessage({ target: { name: "text", value: message.text } });
+  };
+
+  const handleEmojiPickerVisible = () => {
+    setEmojiPickerVisible(!emojiPickerVisible);
+  };
 
   return (
     <div className="h-[10%] flex items-center gap-3 px-2 md:px-10 ">
@@ -15,7 +29,24 @@ const MessageBox = () => {
         size={30}
         title="emoji"
         className="cursor-pointer text-shaded hover:text-black"
+        onClick={handleEmojiPickerVisible}
       />
+      {emojiPickerVisible && 
+      <div className="z-10" id="emojiPicker"
+      style={
+        {
+          position: 'absolute',
+          top: '59%',
+          zIndex: 1
+        }
+      }>
+        <Picker 
+          data={data} 
+          onEmojiSelect={handleEmojiSelect}
+          onClickOutside={handleEmojiPickerVisible}
+          theme="light"
+        /> 
+      </div>}
 
       <label className="flex flex-col items-center justify-center ">
         <MdOutlineAttachment
@@ -36,6 +67,7 @@ const MessageBox = () => {
           name="text"
           type="text"
           className="w-full h-10 text-lg border-none rounded-full outline-none indent-2 bg-slate-100 "
+          id="messageBox"
           placeholder="Aa"
         />
 
